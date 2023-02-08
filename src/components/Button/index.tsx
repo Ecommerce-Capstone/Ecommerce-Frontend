6713e29610edb83f71eb270f55cfab31f3fb41bf
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-02-08 16:31:54 +0700

    slicing auth page

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,8 @@
 const Button = (props: any) => {
     return (
         <>
-            <button type="submit" className="absolute top-0 right-0 p-2.5 px-9 text-sm font-medium text-white bg-sky-900 rounded-lg border border-sky-900 hover:bg-blue-900">
-                <span>Search</span>
+            <button type="submit" className={`p-2.5 px-9 text-sm font-medium text-white bg-sky-900 rounded-lg border border-sky-900 hover:bg-blue-900 ${props.className}`}>
+                {props.children}
             </button>
         </>
     )
---
+++ src/pages/auth/signup.tsx
@@ -0,0 +1,28 @@
+import Layout from "@/components/Layout";
+import Button from "@/components/Button";
+import BasicInput from "@/components/Layout/BasicInput";
+import Link from "next/link";
+
+const SigninPage = () => {
+    return (
+        <>
+            <Layout>
+                <div className="container w-full px-5 py-24 mx-auto flex justify-center">
+                    <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0 relative z-10 shadow-xl">
+                        <h2 className="text-gray-900 text-lg mb-1 font-medium title-font text-center">Register to our app</h2>
+                        <BasicInput label="Username" type="text" />
+                        <BasicInput label="Email" type="email" />
+                        <BasicInput label="Password" type="password" />
+                        <BasicInput label="Repeat Password" type="password" />
+                        <Button >Signup</Button>
+                        <p className="text-xs text-gray-500 mt-3">Have an account? <Link href="/auth/signin" className="font-bold" >Signin</Link></p>
+                    </div>
+                </div>
+            </Layout>
+
+
+        </>
+    )
+}
+
+export default SigninPage;
