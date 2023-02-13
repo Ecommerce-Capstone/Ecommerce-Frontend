c2ab1cb59a8bc5bb46409fdacf666e2529ff0421
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-02-13 16:15:32 +0700

    fix: error build

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,12 +1,11 @@
 import { withAuth } from "next-auth/middleware"
-const _ = require("lodash")
 
 // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 export default withAuth({
     callbacks: {
         authorized({ req, token }) {
             // `/admin` requires admin role
-            if (_.includes(req.nextUrl.pathname, "/admin")) {
+            if (req.nextUrl.pathname.includes("/admin")) {
                 return token?.user?.role === "admin"
             }
             // `/me` only requires the user to be logged in
